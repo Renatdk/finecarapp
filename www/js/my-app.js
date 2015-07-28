e34e500a50a650b741d731143dc045e55560c69f
Дарыбаев Ренат  Кайратович <renatd.k@gmail.com>
2015-07-28 08:48:47 +0000

    add

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -32,19 +32,20 @@ moment.locale('ru');
 
 if(!navigator.onLine){
     myApp.alert("Нет доступка к Интернет! Подключитесь к сети и перезапустите приложение!");
-}
+} 
+
 
     // Push Notification begin
 myApp.onPageInit('index', function (page) {
     
     var pushNotification;
-    
-    document.addEventListener('deviceready', function () {
-        $("#app-status-ul").append('<li>deviceready event received</li>');
+    console.log("aaa");
+    // document.addEventListener('deviceready', function () {
+        $$("#app-status-ul").append('<li>deviceready event received</li>');
         
 		document.addEventListener("backbutton", function(e)
 		{
-        	$("#app-status-ul").append('<li>backbutton event received</li>');
+        	$$("#app-status-ul").append('<li>backbutton event received</li>');
   			
   			if( $("#home").length > 0)
 			{
@@ -62,7 +63,7 @@ myApp.onPageInit('index', function (page) {
 		try 
 		{ 
         	pushNotification = window.plugins.pushNotification;
-      $("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
+      $$("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
         	if (device.platform == 'android' || device.platform == 'Android' ||
                     device.platform == 'amazon-fireos' ) {
 	pushNotification.register(successHandler, errorHandler, {"senderID":"730696176510","ecb":"onNotification"});		// required!
@@ -76,12 +77,12 @@ myApp.onPageInit('index', function (page) {
 			txt+="Error description: " + err.message + "\n\n"; 
 			alert(txt); 
 		} 
-    }, true); 
+    // }, true); 
     
     // handle APNS notifications for iOS
     function onNotificationAPN(e) {
         if (e.alert) {
-             $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+             $$("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
              // showing an alert also requires the org.apache.cordova.dialogs plugin
              navigator.notification.alert(e.alert);
         }
@@ -99,14 +100,14 @@ myApp.onPageInit('index', function (page) {
     
     // handle GCM notifications for Android
     function onNotification(e) {
-        $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+        $$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
         
         switch( e.event )
         {
             case 'registered':
 			if ( e.regid.length > 0 )
 			{
-				$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+				$$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
 				// Your GCM push server needs to know the regID before it can push to this device
 				// here is where you might want to send it the regID for later use.
 				console.log("regID = " + e.regid);
@@ -118,7 +119,7 @@ myApp.onPageInit('index', function (page) {
             	// you might want to play a sound to get the user's attention, throw up a dialog, etc.
             	if (e.foreground)
             	{
-					$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+					$$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
 				      
 				        // on Android soundname is outside the payload. 
 			                // On Amazon FireOS all custom attributes are contained within payload
@@ -132,24 +133,24 @@ myApp.onPageInit('index', function (page) {
 				else
 				{	// otherwise we were launched because the user touched a notification in the notification tray.
 					if (e.coldstart)
-						$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+						$$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
 					else
-					$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+					$$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
 				}
 					
-				$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+				$$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
                 //android only
-				$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+				$$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
                 //amazon-fireos only
-                $("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
+                $$("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
             break;
             
             case 'error':
-				$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+				$$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
             break;
             
             default:
-				$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+				$$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
             break;
         }
     }
