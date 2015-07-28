bb86052f0decc19eabc98e52bce38a5da331a655
Дарыбаев Ренат  Кайратович <renatd.k@gmail.com>
2015-07-28 10:44:47 +0000

    add

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -40,11 +40,11 @@ if(!navigator.onLine){
     var pushNotification;
     
     document.addEventListener('deviceready', function () {
-        $$("#app-status-ul").append('<li>deviceready event received</li>');
+        // $$("#app-status-ul").append('<li>deviceready event received</li>');
         
 		document.addEventListener("backbutton", function(e)
 		{
-        	$$("#app-status-ul").append('<li>backbutton event received</li>');
+        // 	$$("#app-status-ul").append('<li>backbutton event received</li>');
   			
   			if( $("#home").length > 0)
 			{
@@ -65,7 +65,7 @@ if(!navigator.onLine){
       $$("#app-status-ul").append('<li>registering ' + device.platform + '</li>');
         	if (device.platform == 'android' || device.platform == 'Android' ||
                     device.platform == 'amazon-fireos' ) {
-	pushNotification.register(successHandler, errorHandler, {"senderID":"730696176510","ecb":"onNotification"});		// required!
+	            pushNotification.register(successHandler, errorHandler, {"senderID":"730696176510","ecb":"onNotification"});		// required!
 			} else {
             	pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});	// required!
         	}
@@ -74,7 +74,10 @@ if(!navigator.onLine){
 		{ 
 			txt="There was an error on this page.\n\n"; 
 			txt+="Error description: " + err.message + "\n\n"; 
-			alert(txt); 
+			 myApp.addNotification({
+                title: 'FineCar',
+                message: 'Error:'+txt
+            });
 		} 
     }, false); 
 
@@ -99,53 +102,62 @@ if(!navigator.onLine){
     
     // handle GCM notifications for Android
     function onNotification(e) {
-        $$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+        // $$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
         
         switch( e.event )
         {
             case 'registered':
 			if ( e.regid.length > 0 )
 			{
-				$$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+				// $$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
 				// Your GCM push server needs to know the regID before it can push to this device
 				// here is where you might want to send it the regID for later use.
-				console.log("regID = " + e.regid);
+				// console.log("regID = " + e.regid);
+				
 			}
             break;
             
             case 'message':
             	// if this flag is set, this notification happened while we were in the foreground.
             	// you might want to play a sound to get the user's attention, throw up a dialog, etc.
-            	if (e.foreground)
-            	{
-					$$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+    //         	if (e.foreground)
+    //         	{
+				// 	$$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
 				      
-				        // on Android soundname is outside the payload. 
-			                // On Amazon FireOS all custom attributes are contained within payload
-			                var soundfile = e.soundname || e.payload.sound;
-			                // if the notification contains a soundname, play it.
-			                // playing a sound also requires the org.apache.cordova.media plugin
-			                var my_media = new Media("/android_asset/www/"+ soundfile);
+				//         // on Android soundname is outside the payload. 
+			 //               // On Amazon FireOS all custom attributes are contained within payload
+			 //               var soundfile = e.soundname || e.payload.sound;
+			 //               // if the notification contains a soundname, play it.
+			 //               // playing a sound also requires the org.apache.cordova.media plugin
+			 //               var my_media = new Media("/android_asset/www/"+ soundfile);
 
-					my_media.play();
-				}
-				else
-				{	// otherwise we were launched because the user touched a notification in the notification tray.
-					if (e.coldstart)
-						$$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-					else
-					$$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-				}
+				// 	my_media.play();
+				// }
+				// else
+				// {	// otherwise we were launched because the user touched a notification in the notification tray.
+				// 	if (e.coldstart)
+				// 		$$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+				// 	else
+				// 	$$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+				// }
 					
-				$$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-                //android only
-				$$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-                //amazon-fireos only
-                $$("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
+				// $$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+    //             //android only
+				// $$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+    //             //amazon-fireos only
+    //             $$("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
+                 myApp.addNotification({
+                    title: e.title,
+                    message: e.message
+                });
             break;
             
             case 'error':
-				$$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+				// $$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+				 myApp.addNotification({
+                    title: 'FineCar',
+                    message: 'Ошибка:'+e.msg,
+                });
             break;
             
             default:
